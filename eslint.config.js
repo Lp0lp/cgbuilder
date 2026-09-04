454093abc47d60dbb908ad123e8e33fd9beb5b13
Pablo Cardona Perez <pablo.cardona_perez@ens-lyon.fr>
2026-09-04 20:42:18 +0200

    Formatted config files with Prettier
    Added configuration for Dependabot, Husky, and lint-staged
    
    Simplified the npm scripts
    
    Set NodeJS 24 as the required version since that is the current LTS release.

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,15 @@ export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    files: ['eslint.config.js', 'build.mjs', 'vitest.config.ts'],
+    languageOptions: {
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+  },
+  {
     files: ['src/**/*.ts'],
     rules: {
       'no-undef': 'off',
